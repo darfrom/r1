ecee522b1caf3557d73eaaa5a8c31489b904c847
yudindar <dashutka-yudina@yandex.ru>
2017-10-25 20:19:44 +0800

    abra codabra

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require("express");
-const app = express();
+const Web3 = require("web3");
 
-app.get("/", function(req,res){
-	res.send("Hello");
+const contract = require("./contract");
+
+const app = express();
+const web3 = new Web3();
+const { eth } = web3;
+const transaction = "0x9650ebcfbe2b18f4246947648c49def01a439981f3581563cd945a012bd59e77";
+
+web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
+const helloContract = web3.eth.contract(contract.ABI).at(contract.adress);
+
+
+
+
+console.log(web3.eth.coinbase);
+console.log(eth.getTransaction(transaction));
+console.log(eth.blockNumber);
+
+
+app.get("/", function(req, res) {
+  const { personal } = web3;
+
+  personal.unlockAccount(eth.coinbase, "123", 1000);
+
+  res.send(helloContract.setData.sendTransaction("Hell", { from: eth.coinbase }));
 });
 
-app.listen(8000, function(){
-	console.log("Our server");
+app.listen(8000, function() {
+  console.log("Our server on 8000 port");
 });
---
+++ contract.js
@@ -0,0 +1,4 @@
+const ABI = [ { "constant": true, "inputs": [], "name": "getData", "outputs": [ { "name": "", "type": "string", "value": "" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "helloString", "type": "string" } ], "name": "setData", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "owner", "outputs": [ { "name": "", "type": "address", "value": "0x841a38f5a0c459c68866da1676d75a1dca15e8fc" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "newOwner", "type": "address" } ], "name": "transferOwnership", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" } ]
+const adress = "0xbcC67b9b8F0B936660c287823890983F55d4894e"
+
+module.exports = {ABI,adress}
